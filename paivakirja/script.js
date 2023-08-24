dabdc85cd2d83629e4dfe5e31db661f07c8ea614
veeti-kolehmainen <veetikolehmainen@gmail.com>
2023-08-24 23:20:09 +0300

    made a progress bar

--- paivakirja/script.js
+++ paivakirja/script.js
@@ -1,3 +1,8 @@
 const list = document.querySelectorAll("li").length
+const progressBar = document.getElementById("progress")
+const progressBarMax = 30
+const progressPercent = document.querySelector(".progressPercent")
 
+progressBar.value = list
+progressPercent.innerText = Math.round((list / progressBarMax) * 100) + "%"
 console.log(list)
